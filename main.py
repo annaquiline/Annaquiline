108a1a80732e798e5f66931d314b09abb47bd46d
annaquiline <treesajoseann@gmail.com>
2024-10-09 11:00:29 +0000

    ann

---
+++ main.py
@@ -0,0 +1,22 @@
+print("hello world")
+a=23
+b=34.69
+c="Ann"
+d=True
+print(type(a))
+print(type(c))
+name=input("enter ur name :")
+age=int(input('Enter ur name : '))
+class_=int(input("enter ur class :"))
+print(name)
+print("my name is:",name)
+print(float(input("enter a floating no :")))
+print("my age is :",age)
+if age==class_:
+    print("both r same")
+    age+=1
+elif age>3:
+    print(age)
+else:
+    print("thankU")
+print("/du//")
